8a027221311ecd632ea9abf3502e434229722910
Valentin Thirion <valentinthirion@gmail.com>
2015-01-02 11:14:40 +0100

    First working version

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Product list search improvements",
+    'version': '1.0',
+    'depends': ['product', 'sale'],
+    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Product Management',
+    'description': """
+    This module adds some searching facilities inside the product list.
+
+	It adds:
+	- the possibility to search on internal reference
+	- the possibility to search with a barcode reader using the EAN13 code
+    """,
+    'data': ['product_view.xml',],
+    'demo': [],
+}
